f1e35f2c276ec95a5675bf821ad088d6a1dd5ef9
Nakajima <nakajima.jun@gmail.com>
2019-02-12 00:03:52 +0900

    2019/02/12 committed.

--- ShakeMeSilly/ShakeMeSilly/ViewController.h
+++ ShakeMeSilly/ShakeMeSilly/ViewController.h
@@ -8,8 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSTimer *timer;
+    int timeInt;
+    int scoreInt;
+    int gameMode;
+    int imageInt;
+}
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
 
+- (IBAction)startGame:(id)sender;
 
 @end
 
--- ShakeMeSilly/ShakeMeSilly/ViewController.m
+++ ShakeMeSilly/ShakeMeSilly/ViewController.m
@@ -17,7 +17,64 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    timeInt = 10;
+    scoreInt = 0;
+    gameMode = 0;
+    imageInt = 1;
+    
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+    
+    
 }
 
+
+- (IBAction)startGame:(id)sender {
+    if (timeInt == 10) {
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateCounter) userInfo:nil repeats:YES];
+        gameMode = 1;
+        
+        self.startButton.enabled = NO;
+        self.startButton.alpha = 0.25;
+    } else if (timeInt == 0) {
+        timeInt = 10;
+        scoreInt = 0;
+        imageInt = 1;
+        self.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"Face%i", imageInt]];
+        self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+        [self.startButton setTitle:@"Start Game" forState:UIControlStateNormal];
+    }
+    
+}
+
+- (void)updateCounter {
+    timeInt -= 1;
+    self.timeLabel.text = [NSString stringWithFormat:@"%i", timeInt];
+    
+    if (timeInt == 0) {
+        [timer invalidate];
+        gameMode = 0;
+        
+        self.startButton.enabled = YES;
+        self.startButton.alpha = 1.0;
+        [self.startButton setTitle:@"ReStart" forState:UIControlStateNormal];
+    }
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (event.subtype == UIEventSubtypeMotionShake) {
+        if (gameMode == 1) {
+            scoreInt += 1;
+            self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+            imageInt += 1;
+            self.imageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"Face%i", imageInt]];
+            if (imageInt == 12) {
+                imageInt = 0;
+            }
+        }
+    }
+}
 
 @end
